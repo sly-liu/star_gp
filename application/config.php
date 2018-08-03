6f44d13908d34e965d7b02cabba9b9d48ba3cc24
sly <164439652@qq.com>
2018-08-03 17:12:56 +0800

    commit

--- application/config.php
+++ application/config.php
@@ -236,4 +236,9 @@ return [
         'var_page'  => 'page',
         'list_rows' => 15,
     ],
+
+    //视图输出字符串内容替换
+    'view_replace_str'       => [
+        '__PUBLIC__'  => '/public/static',
+    ],
 ];
